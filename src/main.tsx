78b3533eb485f66278f9c63a2b75f788f17480e9
iangopenbusinessai <iangopenbusinessai@gmail.com>
2026-06-15 02:14:57 -0700

    potential change?

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+document.addEventListener('dragstart', e => e.preventDefault(), true)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
